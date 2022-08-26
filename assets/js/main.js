edd16bd690138b08097899cd12741118143ce414
WhyNotSolar <phamthanh240818@gmail.com>
2022-08-26 09:42:37 +0700

    SolLinks Tracking Demo

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', () => {
+    const toast = new bootstrap.Toast(toastLiveExample)
+
+    toast.show()
+  })
+}
+
+var loaderCpn = document.getElementById("preloader")
+loaderCpn.style.animation = "loadingCpn ease 0s"
+window.addEventListener("load", function(){
+  this.setTimeout(function(){
+    loaderCpn.style.display = "none"
+    loaderCpn.style.animation = "loadedCpn ease 2s"
+  }, 2500)
+})
